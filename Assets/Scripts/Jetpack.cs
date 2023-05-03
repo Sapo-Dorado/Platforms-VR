ed62b1d6290528e3a38e7e3c303b77724be9dc78
Nicholas Brown <countdooku.steak@gmail.com>
2023-05-03 14:23:00 -0700

    add fuel

--- Assets/Scripts/Jetpack.cs
+++ Assets/Scripts/Jetpack.cs
@@ -1,16 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
 
 public class Jetpack : MonoBehaviour
 {
     public ActionBasedController rightController;
     public ActionBasedController leftController;
+    public TMP_Text fuelIndicator;
+    public float startingFuel;
     public float acceleration;
     public float maxSpeed;
 
     private Rigidbody body;
+    private float fuel = 0;
 
     Vector3 getForce() {
         Vector3 dir = new Vector3(0,0,0);
@@ -24,20 +29,30 @@ public class Jetpack : MonoBehaviour
     }
 
     void applyForce() {
-        Vector3 force = getForce();
-        body.AddForce(force, ForceMode.Acceleration);
-        float newSpeed = Vector3.Magnitude(body.velocity + force);
-        if(newSpeed > maxSpeed) {
-            Vector3 diff = Vector3.Normalize(body.velocity + force) * (newSpeed - maxSpeed);
-            body.AddForce(-diff, ForceMode.Acceleration);
+        if (fuel > 0) {
+            Vector3 force = getForce();
+            if(Vector3.Magnitude(force) > 0) {
+                updateFuel(-1 * Time.deltaTime);
+                body.AddForce(force, ForceMode.Acceleration);
+                float newSpeed = Vector3.Magnitude(body.velocity + force);
+                if(newSpeed > maxSpeed) {
+                    Vector3 diff = Vector3.Normalize(body.velocity + force) * (newSpeed - maxSpeed);
+                    body.AddForce(-diff, ForceMode.Acceleration);
+                }
+            }
         }
+    }
+
+    void updateFuel(float amount) {
+        fuel += amount;
+        fuelIndicator.SetText(String.Format("Fuel: {0}", fuel.ToString("F2")));
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        updateFuel(startingFuel);
         body = transform.GetComponentInParent<Rigidbody>();
-
     }
 
     // Update is called once per frame
